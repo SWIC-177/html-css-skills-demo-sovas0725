7428bed6cf44b6a65aa2e9a25f30dd3f0a026ac5
Shane Sova <shane.sova@student.swic.edu>
2024-05-06 11:17:23 -0500

    Add event listeners to each input field, such that upon losing focus (`blur` event) on the input fields, will show the error

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const formEntry = [
     ...Array.from(document.querySelectorAll('input')),
-    document.querySelector('textarea'),
-    document.querySelector('button')
-];
+    document.querySelector('select'),
+    document.querySelector('textarea')
+]
 console.log(formEntry);
+
+const submitButton = document.querySelector('button[type="submit"]');
+const resetButton = document.querySelector('button[type="reset"]');
+
+formEntry.forEach((input) => {
+    input.addEventListener('blur', (event) => {
+        console.log("blur event", event.target.id);
+    });
+});
