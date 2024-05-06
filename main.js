30cdecb246261ccec84b4eba99c3daf952078382
Shane Sova <shane.sova@student.swic.edu>
2024-05-06 09:55:52 -0500

    'Select' the submit button.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const formEntry = [
     ...Array.from(document.querySelectorAll('input')),
-    document.querySelector('textarea')
+    document.querySelector('textarea'),
+    document.querySelector('button')
 ];
 console.log(formEntry);
